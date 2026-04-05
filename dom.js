0f99101ba798ac5bc196831de67584dd5f2bd7f1
Liver96Q <121767304+IFOR1607@users.noreply.github.com>
2026-04-05 15:08:30 +0700

    nambah style

---
+++ dom.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('body');
+const button = document.querySelector('button');
+
+function change() {
+ // Kita cek: "Apakah background saat ini putih?"
+    if (body.style.backgroundColor === 'white' || body.style.backgroundColor === '') {
+        // Jika iya (atau masih kosong), kita buat jadi Dark Mode
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+        button.style.backgroundColor = 'white';
+        button.style.color = 'black';
+    } else {
+        // Jika ternyata sudah hitam, kita kembalikan ke Light Mode
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+        button.style.backgroundColor = 'black';
+        button.style.color = 'white';
+    }
+}
